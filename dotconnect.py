118da4cc7f7d0258c150d786ee59018d913aa310
techiehubb <rakeshvsnrs99@gmail>
2023-05-29 19:33:37 +0530

    Changes made inside

--- dotconnect.py
+++ dotconnect.py
@@ -2,5 +2,7 @@ This is to connect the dots in learning
 
 I am not changing content here ...only for practise....chill
 
-This line is to check SSH keys confirmations
+This line is to check SSH keys confirmation
+
+This line is for practises
 
