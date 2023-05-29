0d3640dba78bcd7d60d983dac4294bead2ba67f0
RakeshVelichety <123568420+RakeshVelichety@users.noreply.github.com>
2023-05-29 13:34:34 +0530

    Update dotconnect.py

--- dotconnect.py
+++ dotconnect.py
@@ -2,5 +2,5 @@ This is to connect the dots in learning
 
 I am not changing content here ...only for practise....chill
 
-This line is to check SSH keys confirmations
+This line is to check SSH keys confirmations 
 
